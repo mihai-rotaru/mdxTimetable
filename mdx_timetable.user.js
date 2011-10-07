1d4e19b6979b432499aaa4914145247cf9809366
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-10-07 22:43:39 +0100

    multiple improvements

--- mdx_timetable.user.js
+++ mdx_timetable.user.js
@@ -24,8 +24,22 @@
             }
     }
 
+    // CSS code to hide unnecessary elements
     var css = '.pagetitlediv, .infotextdiv, .headerwrapperdiv { display: none; }'; 
 
+    // add a border to the timetable
+    css += 'TABLE.datadisplaytable { border-color: #4a6c85; border-style: solid; border-width: 1px; border-spacing: 0px; }';
+
+    // links
+    css += 'A:link { color: #c2e078; text-decoration: none; } A:visited { color: #c2e078;} ';
+
+    // padding
+    css += 'TABLE TD.ddlabel { border-color: #222; border-style:solid; border-width: 2px; padding: 10px; }';
+
+    // table header
+    css += 'TABLE TH.ddheader { width: 30px; height: 30px; vertical-align: middle; }';
+
+    // inject the CSS code into the 'head' element
     try {
         var elmHead, elmStyle;
         elmHead = document.getElementsByTagName('head')[0];
@@ -42,7 +56,36 @@
         //          alert( "document.styleSheets[0]: " + document.styleSheets[0] ); 
     }
 
+    // no need for the banner
     body = document.getElementsByTagName('body')[0];
     body.style.backgroundImage = 'none';
+
+    console.debug("TEST");
+
+    // zebra table
+    timetable = document.getElementsByClassName('datadisplaytable')[0];
+    var rows = timetable.rows;
+    for( var i = 0; i < rows.length; i++ )
+    {
+        var cells = rows[i].cells;
+        if( cells )
+            for( var j = 0; j < cells.length; j++ )
+            {
+                cell = cells[j];
+                if( i%2 == 0 )
+                {
+                    cell.style.backgroundColor = '#badcdd';
+                }
+                else
+                {
+                    cell.style.backgroundColor = '#cbf1f2';
+                }
+                if( cell.attributes[0].value == 'ddlabel' )
+                {
+                    cell.style.backgroundColor = '#212b40';
+                }
+            }
+    }
+    console.info( "END!!!");
 
 }) ();
